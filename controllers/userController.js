17419f7f04fe5a357e9abdccb07d8ee15918fe5c
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-09-22 13:31:36 +0530

    add setCookie utility function

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,6 +6,9 @@ import jwt from "jsonwebtoken";
 import { UserModel } from "../models/userModel.js";
 import ErrorHandler from "../middleware/errorHandler.js"
 
+// set cookie function 
+import { setCookie } from "../utils/setCookie.js";
+
 
 
 
@@ -14,7 +17,7 @@ export const userRegister =  async(req, res, next) => {
         try {
             const {name, email, password} = req.body;
             const isUserExist = await UserModel.findOne({email})
-            if(!isUserExist) return next(new ErrorHandler("something went wrong", 500))
+            // check if the user already exist 
             if(isUserExist) {
                 return res.status(404).json({
                     success: false,
@@ -24,11 +27,10 @@ export const userRegister =  async(req, res, next) => {
             const encryptedPassword = await bcrypt.hash(password, 10);
             const user = await UserModel.create({name, email, password: encryptedPassword})
             if(!user) return next(new ErrorHandler("something went wrong creating user", 500))
-            const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
-            res.status(200).cookie("token", token, {maxAge: 5*60*1000}).json({
-                success: true,
-                user,
-            })
+    
+            // setting cookie and sending res 
+            setCookie(user, res, 200, "user registration successfull ")
+
         } catch (error) {
             next(error)
         }
@@ -44,11 +46,8 @@ export const userLogin = async(req, res, next) => {
         if(!user) return next(new ErrorHandler("user does not exist", 404))
         const passwordVerify = await bcrypt.compare(password, user.password)
         if(!passwordVerify) return next(new ErrorHandler("incorrect passwor", 401))
-        const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
-        res.status(200).cookie("token", token).json({
-            success: true,
-            message: "login successfull"
-        })
+        // setting cookie and sending res 
+        setCookie(user, res, 200, "login successfull")
     } catch (error) {
         next(error)
     }
--- utils/setCookie.js
+++ utils/setCookie.js
@@ -2,7 +2,9 @@ import jwt from "jsonwebtoken";
 
 
 export const setCookie = (user, res, statusCode, message) => {
-    return res.status(statusCode).cookie("token", "flejofe", {
+    console.log(process.env.JWT_SECRECT)
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+    return res.status(statusCode).cookie("token", token, {
             httpOnly: true,
             maxAge: 5 * 60 * 1000,
         }).json({
