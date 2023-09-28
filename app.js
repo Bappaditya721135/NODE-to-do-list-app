b0b3b491e0bccba36960ac3fb6745c461613e612
Bappaditya sing <104288057+Bappaditya721135@users.noreply.github.com>
2023-09-28 12:22:03 +0530

    add cors

--- app.js
+++ app.js
@@ -26,9 +26,11 @@ app.use("/api/v1/task", taskRouter)
 
 // error handler 
 app.use(errorHandler)
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+// }))
+
+app.use(cors())
 
